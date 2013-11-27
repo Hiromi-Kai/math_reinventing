d27e473cf7dae84a04d3ab1c770ac9c9814feff0
hiromi_kai <pie05041008@gmail.com>
2013-11-27 21:20:54 +0900

    ラムダ式を使って式を外部化

--- bisection_method.rb
+++ bisection_method.rb
@@ -1,22 +1,24 @@
-
-module BisectionMethod
-  EPS=0.001
-  class <<self
-    def func(x)
-      x**3+x-1
-    end
-    def main_logic(a,b)
-      loop do
-        c=((a+b).to_f/2)
-        if func(c)*a<0
-          b=c
-        else
-          a=c
-        end
-        return c if (a-b).abs < EPS
+# coding:utf-8
+#二分法
+class BisectionMethod
+  def initialize(func,eps=0.001)
+    @eps=eps
+    set_function(func)
+  end
+  def set_function(func)
+    @func=func
+  end
+  def main_logic(a,b)
+    loop do
+      c=((a+b).to_f/2)
+      if @func.call(c)*@func.call(a)<0
+        b=c
+      else
+        a=c
       end
+      return c if (a-b).abs < @eps
     end
   end
 end
-
-p BisectionMethod.main_logic(0,1)
+bm=BisectionMethod.new(->(x){x**3+x-1})
+p bm.main_logic(0,1)
