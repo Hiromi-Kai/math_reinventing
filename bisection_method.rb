39b6f7e9e9953fe67a5b5ed8d7ca0cd6964d8810
hiromi_kai <pie05041008@gmail.com>
2013-11-27 21:44:34 +0900

    メソッドのリファクタリング実行

--- bisection_method.rb
+++ bisection_method.rb
@@ -5,23 +5,28 @@ class BisectionMethod
     @eps=eps
     set_function(func)
   end
+  #導出対象となる関数をラムダ式で代入する
   def set_function(func)
     @func=func
   end
+  #足して2で割る
+  def get_bisection(a,b)
+    (a+b).to_f/2
+  end
+  #方程式f(x)のf(num1)*f(num2)が負を示すか否か
+  def is_minus_product?(num1,num2)
+    @func.call(num1)*@func.call(num2)<0
+  end
+  #解の誤差にまで値が近づいたかどうか
+  def is_convergence?(a,b)
+    (a-b).abs < @eps
+  end
   def main_logic(a,b)
     loop do
-      c=((a+b).to_f/2)
-      if @func.call(c)*@func.call(a)<0
-        b=c
-      else
-        a=c
-      end
-      return c if (a-b).abs < @eps
+      c=get_bisection(a,b)
+      b=c if is_minus_product?(a,c)
+      a=c if is_minus_product?(b,c)
+      return c if is_convergence?(a,b)
     end
   end
 end
-bm=BisectionMethod.new(->(x){x**3+x-1})
-p bm.main_logic(0,1)
-
-bm=BisectionMethod.new(->(x){(x-3)*(x+2)})
-p bm.main_logic(0,10)
