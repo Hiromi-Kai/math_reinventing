3fa90dc9ee26227013f651a4bba6d4dcd082ca42
hiromi_kai <pie05041008@gmail.com>
2013-11-27 22:56:52 +0900

    ニュートン法追加

---
+++ newtons_method.rb
@@ -0,0 +1,29 @@
+# coding:utf-8
+#ニュートン法
+
+class NewtonsMethod
+  def initialize(func,derived_func,eps=0.001)
+    @eps=eps
+    @func=func
+    @derived_func=derived_func
+  end
+  #近似値導出
+  def get_approximation(num)
+    num-@func.call(num).to_f/@derived_func.call(num).to_f
+  end
+  #解の誤差にまで値が近づいたかどうか
+  def is_convergence?(a,b)
+    (a-b).abs < @eps
+  end
+  def main_logic(num)
+    loop do
+      approximation=get_approximation(num)
+      convergence=is_convergence?(num,approximation)
+      num=approximation unless convergence
+      return approximation if convergence
+    end
+  end
+end
+
+nm=NewtonsMethod.new(->(x){x**3+x-1},->(x){3*(x**2)+1},0.001)
+p nm.main_logic(1)
