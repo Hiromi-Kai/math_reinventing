477014af0c45206e66113fdfe1c2820f1974743a
hiromi_kai <pie05041008@gmail.com>
2013-11-27 21:47:53 +0900

    2分法テスト追加

--- bisection_method_spec.rb
+++ bisection_method_spec.rb
@@ -1,10 +1,14 @@
-require 'rspec'
+require './bisection_method'
 
-describe 'My behaviour' do
 
-  it 'should do something' do
+describe '2分法テスト' do
 
-    #To change this template use File | Settings | File Templates.
-    true.should == false
+  it '(x-3)*(x+2)は3付近で解をとる' do
+    bm=BisectionMethod.new(->(x){(x-3)*(x+2)},0.001)
+    bm.main_logic(0,10).should be_within(0.001).of(3)
+  end
+  it '(x-1)*(x+5)は1付近で解をとる' do
+    bm=BisectionMethod.new(->(x){(x-1)*(x+5)},0.001)
+    bm.main_logic(0,10).should be_within(0.001).of(1)
   end
 end
