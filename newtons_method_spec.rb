bd9b27867e57118a5f20677c48066e9e27e4cd46
Hiromi Kai <pie05041008@gmail.com>
2014-07-07 15:35:24 +0900

    Update newtons_method_spec.rb
    解のアサーションに指定した値が解とズレ過ぎだったので数値を修正

--- newtons_method_spec.rb
+++ newtons_method_spec.rb
@@ -4,8 +4,8 @@ require './newtons_method'
 describe 'ニュートン法テスト' do
 
   it 'x**3+x-1は0.682付近で解をとる' do
-    nm=NewtonsMethod.new(->(x){x**3+x-1},->(x){3*x**2+1},0.000001)
-    nm.main_logic(1).should be_within(0.000001).of(0.682)
+    nm=NewtonsMethod.new(->(x){x**3+x-1},->(x){3*x**2+1},0.0000001)
+    nm.main_logic(1).should be_within(0.0000001).of(0.6823278)
   end
   it '(x-1)*(x+5)は1付近で解をとる' do
     nm=NewtonsMethod.new(->(x){(x-1)*(x+5)},->(x){2*x+4},0.000001)
